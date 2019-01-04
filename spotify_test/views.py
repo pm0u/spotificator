f0869c20157b31791255ab0ddc0ec8abf3baa75f
Paul Mourer <paul.mourer@gmail.com>
2019-01-04 16:48:12 -0700

    spotify oauth request working. sends to success landing page & able to grab code from url

--- spotify_test/views.py
+++ spotify_test/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect,HttpRequest
 from spotify_test.config import client_id,client_secret
 from django.urls import reverse
+import urllib
 
 # Create your views here.
 
 
 
 def index(request):
-    redirect_uri = reverse('spotify_test:success')
+    redirect_uri = urllib.parse.quote(request.build_absolute_uri() + 'success/')
     scope='playlist-read-private'
     response_type='code'
-    return render(request, 'spotify_test/index.html', {'client_id':client_id, 'redirect_uri':redirect_uri, 'scope':scope, 'response_type':response_type })
+    show_dialog='true'
+    return render(request, 'spotify_test/index.html', {'client_id':client_id, 'redirect_uri':redirect_uri, 'scope':scope, 'response_type':response_type, 'show_dialog':show_dialog })
 
 def success(request):
-    return HttpResponse("you've made it")
+    code = request.GET.get('code')
+    return HttpResponse(f'we\'re in bitch <br> here\'s your code ya filthy animal: <br> {code}')
