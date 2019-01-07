981c429f5957bb5b283c7478f91988b5982ff016
Paul Mourer <paul.mourer@gmail.com>
2019-01-06 18:57:05 -0700

    Oauth working, got code and access token.
    have all working, but i think this will be best handled by front end.
    and then handle all data on front end, only passing to back end as
    needed (to save in DB, etc)

--- spotify_test/views.py
+++ spotify_test/views.py
@@ -3,13 +3,12 @@ from django.http import HttpResponse,HttpResponseRedirect,HttpRequest
 from spotify_test.config import client_id,client_secret
 from django.urls import reverse
 import urllib
+import requests
 
 # Create your views here.
 
 
-
 def index(request):
-    redirect_uri = urllib.parse.quote(request.build_absolute_uri() + 'success/')
     scope='playlist-read-private'
     response_type='code'
     show_dialog='true'
@@ -17,4 +16,11 @@ def index(request):
 
 def success(request):
     code = request.GET.get('code')
-    return HttpResponse(f'we\'re in bitch <br> here\'s your code ya filthy animal: <br> {code}')
+    redirect_uri = urllib.parse.quote(request.build_absolute_uri() + 'success/')
+    access_token_request_data = {
+        'grant_type':'authorization_code',
+        'code':code,
+        'redirect_uri':redirect_uri
+    }
+    r = requests.post(url='https://accounts.spotify.com/api/token', data=access_token_request_data)
+    return HttpResponse(f'we\'re in bitch <br> here\'s your code ya filthy animal: <br> {code} <br> and the access token {r}')
